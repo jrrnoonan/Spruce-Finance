1307b8f2dd613d39caca15686929774e20259da0
jrrnoonan <jrrnoonan@gmail.com>
2018-11-08 11:23:43 -0400

    wrote basic javascript and linked to index page

---
+++ javascript.js
@@ -0,0 +1,5 @@
+var requestURL = 'https://jrrnoonan.github.io/Spruce-Finance/';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
